2003e2b135033a168b1717830bec02d3a3d1795c
arjunevolvan <arjunk@evolvan.com>
2021-01-28 11:08:46 +0530

    typescript function done

--- public/app.js
+++ public/app.js
@@ -1,5 +1,72 @@
 "use strict";
-console.log('app.js file');
+//Anonymous function
+var show = function () {
+    console.log('Hello World');
+};
+show();
+//Named function
+function add(a, b) {
+    var c = a + b;
+    console.log(c);
+}
+add(10, 20);
+// function parameter
+function display(name) {
+    console.log("Hello" + " " + name);
+}
+display('arjun');
+//default parameters
+function greet(name, greeting) {
+    if (name === void 0) { name = "dhruve"; }
+    if (greeting === void 0) { greeting = 'hello'; }
+    return greeting + " " + name;
+}
+console.log(greet());
+//Rest Parameters
+function greets(greeting) {
+    var name = [];
+    for (var _i = 1; _i < arguments.length; _i++) {
+        name[_i - 1] = arguments[_i];
+    }
+    return greeting + " " + name;
+}
+console.log(greets('hello', 'john', 'smith', 'suresh'));
+// using anonymous rest parameter and arrow function 
+var a = function (greeting) {
+    var name = [];
+    for (var _i = 1; _i < arguments.length; _i++) {
+        name[_i - 1] = arguments[_i];
+    }
+    return greeting + " " + name;
+};
+console.log(a('hello', 'john', 'smith', 'suresh'));
+//optional parameter function
+function showName(name, greeting) {
+    if (greeting == undefined) {
+        greeting = "Hello";
+    }
+    return greeting + " " + name;
+}
+console.log(showName('arjun'));
+// for(let i = 0; i<10; i++){
+//     console.log(i)
+// }
+// var arr = [12,23,45,45,78,55];
+// for(let i = 0; i<=arr.length; i++){
+//     console.log(arr[i]);
+// }
+// let i:number = 1;
+// while(i<=10){
+//     console.log(i);
+//     if(i%5==0){
+//         break;
+//     }
+//     i++;
+// }
+// do{
+//     console.log(i);
+//     i++
+// }while(i<=10);
 //enum
 //  enum days{
 //     mon = 'mon'
